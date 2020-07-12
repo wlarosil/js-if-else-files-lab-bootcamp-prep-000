efdc82573dfa2a5f0a05954869cb6e3433c6e34f
Wanda Larosiliere <wglarosiliere@gmail.com>
2020-07-12 16:45:23 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ var height = "74"
 if (name === "Susan") {
     console.log("The name is correct")
 }
-else console.log
+else console.log()
 // Don't worry about this
 module.exports = { name, height
 }
